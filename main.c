dfccd8480038b58ad1cd3cb47c7e54a6d99ae4ae
mikek1337 <mikekegnlyew@gmail.com>
2022-09-05 21:27:08 +0300

    help

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * init_shell - starts the shell
+ */
+void init_shell(void)
+{
+	printf("#cisfun$ ");
+}
+
 /**
  * main - start of the program
  * Return: int
  */
 int main(void)
 {
+	char *args[2];
+	char *command = malloc(sizeof(char) * 100);
+	extern char **environ;
+	init_shell();
+	while (1)
+	{
+		scanf("%s", command);
+		args[0] = command;
+		if (execve(args[0], args, environ) == -1)
+			printf("%s: No such file or directory\n",arg);
+		else
+			printf(" ");
+
+		init_shell();
+	}
 	return (0);
 }
